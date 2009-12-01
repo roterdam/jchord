79abe82790abc20469418f92bb31f7b5fdd0b955
jozef.babjak <jozef.babjak@fd19b882-93bd-11dd-bc7b-59c4a08ba726>
2009-12-01 20:00:27 +0000

    Documenting and adding proper sychronization. Without synchronization, this implementation suffers has severe race conditions if used in multithreaded environment.

--- src/util/java/chord/util/Executor.java
+++ src/util/java/chord/util/Executor.java
@@ -9,30 +9,66 @@ import java.util.List;
 import java.util.ArrayList;
 
 /**
- * 
  * @author Mayur Naik (mhn@cs.stanford.edu)
  */
-public final class Executor {
+public final class Executor { // TODO: MIGHT BE REPLACED BY USING java.util.concurrent, WHICH ALLOWS MUCH BETTER CONCURRENCY CONTROL MUCH MORE SCALABLE LOCKING.
+
+	/**
+	 * Submitted tasks. This field is {@code null} if the executor is serial.
+	 */
 	private final List<Thread> tasks;
+
+	/**
+	 * A flag indicating that submitted tasks will be executed serially.
+	 */
 	private final boolean serial;
-	public Executor(boolean serial) {
+
+	/**
+	 * Creates a new executor.
+	 *
+	 * @param serial a flag indicating that submitted tasks will be executed serially.
+	 */
+	public Executor(final boolean serial) {
 		this.serial = serial;
 		tasks = serial ? null : new ArrayList<Thread>();
 	}
-	public void execute(Runnable task) {
-		if (tasks == null)
+
+	/**
+	 * Executes given runnable task. If the executor is serial, this method blocks
+	 * until the submitted task is completed. If the executor is not serial, this
+	 * method only starts a new thread for the submitted task and returns immediately.
+	 *
+	 * @param task a task to be executed.
+	 * @throws IllegalArgumentException if {@code task} is {@code null}.
+	 */
+	public synchronized void execute(final Runnable task) {
+		if (task == null) {
+			throw new IllegalArgumentException();
+		}
+		if (serial)
 			task.run();
 		else {
-			Thread t = new Thread(task);
+			final Thread t = new Thread(task);
 			tasks.add(t);
 			t.start();
 		}
 	}
-	public void waitForCompletion() throws InterruptedException {
-		if (tasks == null)
-			return;
-		for (Thread t : tasks)
-			t.join();
-		tasks.clear();
+
+	/**
+	 * Waits for completion of all submitted tasks and returns afterwards.
+	 *
+	 * @throws InterruptedException if any occurs during waiting for tasks completion.
+	 */
+	public synchronized void waitForCompletion() throws InterruptedException {
+		if (!serial) {
+			try {
+				for (final Thread t : tasks) {
+					t.join();
+				}
+			} finally {
+				tasks.clear();
+			}
+		}
 	}
+
 }
