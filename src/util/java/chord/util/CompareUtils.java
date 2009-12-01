bb33cac370e702c6faf57c63b04a1e7aae8f5c22
jozef.babjak <jozef.babjak@fd19b882-93bd-11dd-bc7b-59c4a08ba726>
2009-12-01 20:37:29 +0000

    Adding javadoc of that surprising behavior.

--- src/util/java/chord/util/CompareUtils.java
+++ src/util/java/chord/util/CompareUtils.java
@@ -7,11 +7,31 @@ package chord.util;
 
 /**
  * Object comparison utilities.
- * 
+ *
  * @author Mayur Naik (mhn@cs.stanford.edu)
  */
-public class CompareUtils {
-	public static boolean areEqual(Object a, Object b) {
-		return a == null ? b == null : a.equals(b);
+public final class CompareUtils {
+
+	/**
+	 * Just disables an instance creation of this utility class.
+	 *
+	 * @throws UnsupportedOperationException always.
+	 */
+	private CompareUtils() {
+		throw new UnsupportedOperationException();
 	}
+
+	/**
+	 * Returns {@code true} iff given objects are same by meaning of reference
+	 * or by meaning of {@code equals()} method. The first condition applies that
+	 * this method returns {@code true} also when the both objects are {@code null}.
+	 *
+	 * @param x the first compared object.
+	 * @param y the second compared object.
+	 * @return {@code true} iff given objects are same by meaning of reference or by meaning of {@code equals()} method.
+	 */
+	public static boolean areEqual(final Object x, final Object y) {
+		return x == null ? y == null : x.equals(y);
+	}
+
 }
