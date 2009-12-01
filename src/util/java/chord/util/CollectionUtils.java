bb366fa4603837dbc451df3868b8d3f2f9e10e31
jozef.babjak <jozef.babjak@fd19b882-93bd-11dd-bc7b-59c4a08ba726>
2009-12-01 19:02:08 +0000

    Reimplementing string concatenation using efficient StringBuilder instead of direct string concatenation.

--- src/util/java/chord/util/CollectionUtils.java
+++ src/util/java/chord/util/CollectionUtils.java
@@ -9,44 +9,61 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
+
 /**
  * Collection related utilities.
- * 
+ *
  * @author Mayur Naik (mhn@cs.stanford.edu)
  */
-public class CollectionUtils {
+public final class CollectionUtils {
+
+	/**
+	 * Just disables an instance creation of this utility class.
+	 *
+	 * @throws UnsupportedOperationException always.
+	 */
+	private CollectionUtils() {
+		throw new UnsupportedOperationException();
+	}
+
 	/**
 	 * Determines whether a given collection contains duplicate
 	 * values.
-	 * 
+	 *
+	 * @return true iff the given collection contains duplicate
+	 *         values.
 	 * @param	<T>	The type of the collection elements.
-	 * @param	elems	A collection.
-	 * 
-	 * @return	true iff the given collection contains duplicate
-	 * 			values.
+	 * @param	elements	A collection.
 	 */
-	public static <T> boolean hasDuplicates(List<T> elems) {
-		for (int i = 0; i < elems.size() - 1; i++) {
-			T elem = elems.get(i);
-			for (int j = i + 1; j < elems.size(); j++) {
-				if (elems.get(j).equals(elem))
+	public static <T> boolean hasDuplicates(final List<T> elements) {
+		for (int i = 0; i < elements.size() - 1; i++) {
+			final T element = elements.get(i);
+			for (int j = i + 1; j < elements.size(); j++) {
+				if (elements.get(j).equals(element))
 					return true;
 			}
 		}
 		return false;
 	}
-	public static <T> String toString(Collection<T> a,
-			String begStr, String sepStr, String endStr) {
-		int n = a.size();
-		if (n == 0)
-			return begStr + endStr;
-		Iterator<T> it = a.iterator();
-		String s = begStr + it.next();
-		while (it.hasNext())
-			s += sepStr + it.next();
-		return s + endStr;
+
+	public static <T> String toString(final Collection<T> c, final String prefix, final String separator, final String suffix) {
+		if (c == null) {
+			throw new IllegalArgumentException();
+		}
+		final StringBuilder result = new StringBuilder(prefix);
+		if (c.size() > 0) {
+			final Iterator<T> it = c.iterator();
+			result.append(it.next());
+			while (it.hasNext()) {
+				result.append(separator).append(it.next());
+			}
+		}
+		return result.append(suffix).toString();
 	}
-	public static <T> String toString(Collection<T> a) {
+
+
+	public static <T> String toString(final Collection<T> a) {
 		return toString(a, "", ",", "");
 	}
+
 }
