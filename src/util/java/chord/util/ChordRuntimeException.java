db4fc9de253856fff1262c31d7c06792275e748f
jozef.babjak <jozef.babjak@fd19b882-93bd-11dd-bc7b-59c4a08ba726>
2009-12-01 18:20:55 +0000

    Adding javadoc documentation. Adding complete set of constructors.

--- src/util/java/chord/util/ChordRuntimeException.java
+++ src/util/java/chord/util/ChordRuntimeException.java
@@ -6,16 +6,46 @@
 package chord.util;
 
 /**
- * 
+ * A basic runtime exception for Chord.
+ *
  * @author Mayur Naik (mhn@cs.stanford.edu)
  */
 public class ChordRuntimeException extends RuntimeException {
-	public ChordRuntimeException() { }
-	public ChordRuntimeException(String msg) {
-		super(msg);
+
+	/**
+	 * Creates a new instance.
+	 */
+	public ChordRuntimeException() {
+		super();
 	}
-	public ChordRuntimeException(Throwable ex) {
-		super(ex);
+
+	/**
+	 * Creates a new instance with given message.
+	 *
+	 * @param message an exception message.
+	 */
+	public ChordRuntimeException(final String message) {
+		super(message);
 	}
+
+	/**
+	 * Creates a new instance with given cause.
+	 *
+	 * @param cause an exception cause.
+	 */
+	public ChordRuntimeException(final Throwable cause) {
+		super(cause);
+	}
+
+	/**
+	 * Creates a new instance with given message and cause.
+	 *
+	 * @param message an exception message.
+	 * @param cause   an exception cause.
+	 */
+	public ChordRuntimeException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
+
 }
 
