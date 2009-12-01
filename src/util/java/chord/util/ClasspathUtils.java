fee51a19ac0bc07d379922d38abcdc3a3979bd5d
jozef.babjak <jozef.babjak@fd19b882-93bd-11dd-bc7b-59c4a08ba726>
2009-12-01 18:40:37 +0000

    Refactoring a bit. This can be simplyfied even more; see added TODO.

--- src/util/java/chord/util/ClasspathUtils.java
+++ src/util/java/chord/util/ClasspathUtils.java
@@ -6,46 +6,59 @@
 package chord.util;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Map;
+import java.util.List;
 import java.util.Set;
 
 import org.scannotation.AnnotationDB;
 
 /**
- * 
  * @author Mayur Naik (mhn@cs.stanford.edu)
  */
-public class ClasspathUtils {
-	public static Set<String> getClassNames(String classPath) {
-        ArrayList<URL> list = new ArrayList<URL>();
-        String[] fileNames = classPath.split(File.pathSeparator);
-        for (String fileName : fileNames) {
-            File file = new File(fileName);
-            if (!file.exists()) {
-				System.out.println("WARNING: Ignoring: " + fileName);
-				continue;
-            }
-            try {
-            	list.add(file.toURL());
-            } catch (MalformedURLException ex) {
-            	throw new RuntimeException(ex);
-            }
-        }
-        URL[] urls = new URL[list.size()];
-        list.toArray(urls);
-        AnnotationDB db = new AnnotationDB();
-		db.setIgnoredPackages(new String[0]);
-        try {
-            db.scanArchives(urls);
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
-        }
-        Map<String, Set<String>> index = db.getClassIndex();
-        return index.keySet();
+public final class ClasspathUtils {
+
+	/**
+	 * An empty string array.
+	 */
+	private static final String[] EMPTY_STRING_ARRAY = new String[0];
+
+	/**
+	 * Just disables an instance creation of this utility class.
+	 *
+	 * @throws UnsupportedOperationException always.
+	 */
+	private ClasspathUtils() {
+		throw new UnsupportedOperationException();
 	}
+
+	public static Set<String> getClassNames(final String classPath) {
+		if (classPath == null) {
+			throw new IllegalArgumentException();
+		}
+		final List<URL> list = new ArrayList<URL>();
+		for (final String fileName : classPath.split(File.pathSeparator)) {
+			final File file = new File(fileName);
+			if (!file.exists()) {
+				System.out.println("WARNING: Ignoring: " + fileName); // TODO: REMOVE System.out.println()
+				continue;
+			}
+			try {
+				list.add(file.toURL());
+			} catch (final MalformedURLException ex) {
+				throw new RuntimeException(ex);
+			}
+		}
+		final AnnotationDB db = new AnnotationDB();
+		db.setIgnoredPackages(EMPTY_STRING_ARRAY);
+		try {
+			db.scanArchives(list.toArray(new URL[list.size()]));
+		} catch (final IOException ex) {
+			throw new RuntimeException(ex);
+		}
+		return db.getClassIndex().keySet();
+	}
+
 }
