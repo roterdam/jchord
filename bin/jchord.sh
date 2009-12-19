c1a10a6fa2ea10e7fec8d43dc9eb92a2341475f9
jozef.babjak <jozef.babjak@fd19b882-93bd-11dd-bc7b-59c4a08ba726>
2009-12-19 13:50:00 +0000

    The script is done. Now is the task to ensure that all necessary libraries are placed to release/lib directory when 'release' target is executed. 3rd-party libraries will be easy - it is enough to copy them. But our own libraries need to be build first.

--- bin/jchord.sh
+++ bin/jchord.sh
@@ -1,6 +1,20 @@
 #! /bin/sh
 
 ##
+## Jchord executable script. Do not modify this file in any way.
+## You can still affect the execution in several ways:
+##
+## Select JVM version you want by setting JAVA_HOME environment varibale.
+##
+## Any additional parameters can be passed to Java by setting environment
+## variables JAVA_OPTS and/or JCHORD_OPTS. If boths of them are set and 
+## contains conflicting options, the second one overwrites the first one.  
+##
+## Any command lines argumens passed to this scrip will be passed to 
+## Jchord program without any change. 
+##
+
+##
 ## Detecting java command. If JAVA_HOME is set, we try to use it, 
 ## but verify wheter it is set correctly. If JAVA_HOME is not set, 
 ## we try to find 'java' command in PATH.
@@ -43,8 +57,8 @@ done
 
 export CLASSPATH
 
-echo "Classpath is '${CLASSPATH}'."
-
-exec "${java_cmd}" -version
-
+##
+## SHOW MUST GO ON
+##
+exec "${java_cmd}" ${JAVA_OPTS} ${JCHORD_OPTS} 'chord.project.Project' "${@}"
 
