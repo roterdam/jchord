98aa25f43bdbd42ff5b5d1d8cea697cf1c3cf6ec
mayur.naik <mayur.naik@fd19b882-93bd-11dd-bc7b-59c4a08ba726>
2010-02-22 21:58:47 +0000

    added hedc benchmark, added mhp and thresc testcases, and created different subdirs bench and tests under chord/test for benchmarks and testcases

---
+++ test/tests/thresc1/src/T.java
@@ -0,0 +1,52 @@
+public class T {
+	static {
+		(new Thread() { public void run() { }}).start();
+	}
+	static C g;
+	public static void main(String[] args) {
+		A a = new A();
+		if (args == null) {
+			B b = new B();
+			a.bf = b;
+		} else {
+			B b = new B();
+			C c = new C();
+ 			D d = new D();
+			a.bf = b;	// loc by ap, aw
+			a.cf = c;	// loc by ap, aw
+			b.df = d;	// loc by ap, aw
+			c.df = d;	// loc by ap, aw
+			g = c;
+		}
+		int i, j;
+		{
+			B b = a.bf;	// loc by ap, aw
+			D d = b.df;	// loc by ap, esc by aw
+			i = d.i;	// esc by ap
+		}
+		{
+			C c = a.cf;	// loc by ap, aw
+			D d = c.df;	// esc by ap
+			j = d.i;	// esc by ap
+		}
+		System.out.println(i + j);
+	}
+}
+
+class A {
+	B bf;
+	C cf;
+}
+
+class B {
+	D df;
+}
+
+class C {
+	D df;
+}
+
+class D {
+	int i;
+}
+
