2b5776854bdeaa6a8cade14130a54e046b3b4922
omertripp <omertripp@fd19b882-93bd-11dd-bc7b-59c4a08ba726>
2010-03-11 16:35:05 +0000

    FEAT: - infrastructure for propagating labels in a semi-incremental fashion, plus a class using the infrastructure. should be the basis of all reachability-based abstractions if this works well. still in need of testing.

---
+++ main/src/java/chord/analyses/snapshot/ReachabilityTest.java
@@ -0,0 +1,119 @@
+/**
+ * 
+ */
+package chord.analyses.snapshot;
+
+import gnu.trove.TIntHashSet;
+import gnu.trove.TIntObjectHashMap;
+import gnu.trove.TIntObjectProcedure;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import chord.instr.InstrScheme;
+import chord.project.Chord;
+import chord.project.analyses.LabelBasedAnalysis;
+import chord.util.IndexMap;
+
+/**
+ * @author omert
+ *
+ */
+@Chord(name="dynamic-reach-test")
+public class ReachabilityTest extends LabelBasedAnalysis {
+
+	private static class AllocationSiteLabel implements Label {
+		public final int h;
+		
+		public AllocationSiteLabel(int h) {
+			this.h = h;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + h;
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			AllocationSiteLabel other = (AllocationSiteLabel) obj;
+			if (h != other.h)
+				return false;
+			return true;
+		}
+	}
+	
+	private final TIntObjectHashMap<TIntHashSet> alloc2objects = new TIntObjectHashMap<TIntHashSet>();
+	private IndexMap<String> Emap;
+	
+	@Override
+	public InstrScheme getInstrScheme() {
+		InstrScheme result = super.getInstrScheme();
+		result.setNewAndNewArrayEvent(true, false, true);
+		result.setPutfieldReferenceEvent(true, false, true, true, true);
+		result.setAstoreReferenceEvent(true, false, true, true, true);
+		return result;
+	}
+	
+	@Override
+	public void processNewOrNewArray(int h, int t, int o) {
+		if (o != 0 && h >= 0) {
+			Set<Label> S = new HashSet<Label>(1);
+			S.add(new AllocationSiteLabel(h));
+			object2labels.put(o, S);
+			TIntHashSet T = alloc2objects.get(h);
+			if (T == null) {
+				T = new TIntHashSet();
+				alloc2objects.put(h, T);
+			}
+			T.add(o);
+		}
+	}
+	
+	@Override
+	protected TIntHashSet getRoots(Label l) {
+		assert (l instanceof AllocationSiteLabel);
+		AllocationSiteLabel allocLabel = (AllocationSiteLabel) l;
+		return alloc2objects.get(allocLabel.h);
+	}
+
+	@Override
+	public void initAllPasses() {
+		super.initAllPasses();
+		Emap = instrumentor.getEmap();
+	}
+	
+	@Override
+	public void donePass() {
+		super.donePass();
+		object2labels.forEachEntry(new TIntObjectProcedure<Set<Label>>() {
+			@Override
+			public boolean execute(int arg0, Set<Label> arg1) {
+				System.out.println("Object is: " + arg0);
+				System.out.println("Labels are: ");
+				if (arg1 != null) {
+					for (Label l : arg1) {
+						assert (l instanceof AllocationSiteLabel);
+						AllocationSiteLabel allocLabel = (AllocationSiteLabel) l;
+						String s = Emap.get(allocLabel.h);
+						if (s == null) {
+							System.out.println("\t" + allocLabel.h);
+						} else {
+							System.out.println("\t" + s);
+						}
+					}
+				}
+				return true;
+			}
+		});
+	}
+}
---
+++ main/src/java/chord/project/analyses/LabelBasedAnalysis.java
@@ -0,0 +1,153 @@
+/**
+ * 
+ */
+package chord.project.analyses;
+
+import gnu.trove.TIntHashSet;
+import gnu.trove.TIntIntHashMap;
+import gnu.trove.TIntIntProcedure;
+import gnu.trove.TIntIterator;
+import gnu.trove.TIntObjectHashMap;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import chord.instr.InstrScheme;
+
+/**
+ * @author omert
+ *
+ */
+public abstract class LabelBasedAnalysis extends DynamicAnalysis {
+
+	protected static interface Label {
+	}
+	
+	private class Procedure implements TIntIntProcedure {
+		private final TIntHashSet worklist;
+		private final TIntHashSet visited;
+		private final Set<Label> labels;
+		private final boolean isPos;
+
+		public Procedure(TIntHashSet worklist, TIntHashSet visited, Set<Label> labels, boolean isPos) {
+			this.worklist = worklist;
+			this.visited = visited;
+			this.labels = labels;
+			this.isPos = isPos;
+		}
+		
+		@Override
+		public boolean execute(int arg0, int arg1) {
+			if (arg1 != 0) {
+				for (Label label : labels) {
+					if (isPos) 
+						posLabel(arg1, label);
+					else 
+						negLabel(arg1, label);
+					if (!visited.contains(arg1)) {
+						worklist.add(arg1);
+					}
+				}
+			}
+			return true;
+		}
+	}
+	
+//	private final static int ARRAY_CONTENT = Integer.MIN_VALUE;
+	
+	private InstrScheme instrScheme;
+	private final TIntObjectHashMap<TIntIntHashMap> heapGraph = new TIntObjectHashMap<TIntIntHashMap>();
+	protected final TIntObjectHashMap<Set<Label>> object2labels = new TIntObjectHashMap<Set<Label>>();
+
+	protected abstract TIntHashSet getRoots(Label l);
+	
+	private Set<Label> getLabels(int b) {
+		return object2labels.get(b);
+	}
+	
+	private void posLabel(int o, Label l) {
+		Set<Label> S = object2labels.get(o);
+		if (S == null) {
+			object2labels.put(o, S = new HashSet<Label>(1));
+		}
+		S.add(l);
+	}
+	
+	private void negLabel(int o, Label l) {
+		Set<Label> S = object2labels.get(o);
+		if (S != null) {
+			S.remove(l);
+		}
+	}
+	
+	@Override
+	public InstrScheme getInstrScheme() {
+		if (instrScheme != null) return instrScheme;
+		instrScheme = new InstrScheme();
+		instrScheme.setPutfieldReferenceEvent(false, false, true, true, true);
+		instrScheme.setAstoreReferenceEvent(false, false, true, true, true);
+		return instrScheme;
+	}
+	
+	@Override
+	public void processAstoreReference(int e, int t, int b, int i, int o) {
+		if (e >= 0 && b != 0) {
+			updateHeapGraph(b, i, o);
+		}
+	}
+	
+	@Override
+	public void processPutfieldReference(int e, int t, int b, int f, int o) {
+		if (e >= 0 && b != 0 && f >= 0) {
+			updateHeapGraph(b, f, o);
+		}
+	}
+	
+	private void updateHeapGraph(int b, int f, int o) {
+		TIntIntHashMap M = heapGraph.get(b);
+		if (M == null) {
+			heapGraph.put(b, M = new TIntIntHashMap());
+		}
+		M.put(f, o);
+		Set<Label> labels = getLabels(b);
+		if (labels != null) {
+			if (o == 0) {
+				propagateLabels(o, labels, false);
+				for (Label l : labels) {
+					TIntHashSet roots = getRoots(l);
+					for (TIntIterator it=roots.iterator(); it.hasNext(); ) {
+						propagateLabels(it.next(), Collections.<Label> singleton(l), true);
+					}
+				}
+			} else {
+				propagateLabels(o, labels, true);
+			}
+		}
+	}
+	
+	private void propagateLabels(int o, Set<Label> labels, boolean isPos) {
+		TIntHashSet worklist = new TIntHashSet();
+		TIntHashSet visited = new TIntHashSet();
+		for (Label l : labels) {
+			if (isPos)
+				posLabel(o, l);
+			else
+				negLabel(o, l);
+		}
+		worklist.add(o);
+		while (!worklist.isEmpty()) {
+			TIntIterator it = worklist.iterator();
+			worklist = new TIntHashSet();
+			Procedure proc = new Procedure(worklist, visited, labels, isPos);
+			while (it.hasNext()) {
+				final int next = it.next();
+				visited.add(next);
+				TIntIntHashMap M = heapGraph.get(next);
+				if (M != null) {
+					M.forEachEntry(proc);
+				}
+			}
+		}
+	}
+}
