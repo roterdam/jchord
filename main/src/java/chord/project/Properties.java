a9ba954b95c4d5b5f2ed0f1c800c6b0a5069c2cb
mayur.naik <mayur.naik@fd19b882-93bd-11dd-bc7b-59c4a08ba726>
2010-03-16 04:27:51 +0000

    minor

--- main/src/java/chord/project/Properties.java
+++ main/src/java/chord/project/Properties.java
@@ -153,7 +153,7 @@ public class Properties {
 	public final static String instrSchemeFileName = outRel2AbsPath("chord.instr.scheme.file", "scheme.ser");
 	public final static String crudeTraceFileName = outRel2AbsPath("chord.crude.trace.file", "crude_trace.txt");
 	public final static String finalTraceFileName = outRel2AbsPath("chord.final.trace.file", "final_trace.txt");
-	public final static int maxConstr = Integer.getInteger("chord.max.constr", 500000000);
+	public final static int maxConstr = Integer.getInteger("chord.max.constr", 50000000);
 
 	public static void print() {
 		System.out.println("*** Chord resource properties:");
