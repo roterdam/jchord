da62e3a7a80c8b6092f58e8d47c4659959420104
mayur.naik <mayur.naik@fd19b882-93bd-11dd-bc7b-59c4a08ba726>
2010-06-18 17:38:59 +0000

    minor

--- main/src/java/chord/program/IScope.java
+++ main/src/java/chord/program/IScope.java
@@ -1,8 +1,19 @@
+/*
+ * Copyright (c) 2008-2010, Intel Corporation.
+ * Copyright (c) 2006-2007, The Trustees of Stanford University.
+ * All rights reserved.
+ */
 package chord.program;
 
 import joeq.Class.jq_Method;
 import chord.util.IndexSet;
 
+/**
+ * Generic interface for algorithms computing analysis scope
+ * (reachable classes and methods).
+ *
+ * @author Mayur Naik (mhn@cs.stanford.edu)
+ */
 public interface IScope {
 	public IndexSet<jq_Method> getMethods();
 }
