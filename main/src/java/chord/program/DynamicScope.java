ed4c2a60408491d44477e01f7f1ff744d21d3fba
mayur.naik <mayur.naik@fd19b882-93bd-11dd-bc7b-59c4a08ba726>
2010-06-18 22:34:56 +0000

    forgot to add dynamic scope

---
+++ main/src/java/chord/program/DynamicScope.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2008-2010, Intel Corporation.
+ * Copyright (c) 2006-2007, The Trustees of Stanford University.
+ * All rights reserved.
+ */
+package chord.program;
+
+import java.util.List;
+
+import chord.util.IndexSet;
+ 
+import joeq.Main.HostedVM;
+import joeq.Class.jq_Class;
+import joeq.Class.jq_Array;
+import joeq.Class.jq_Reference;
+import joeq.Class.jq_Method;
+
+/**
+ * 
+ * @author Mayur Naik (mhn@cs.stanford.edu)
+ */
+public class DynamicScope implements IScope {
+	private IndexSet<jq_Method> methods;
+	public IndexSet<jq_Method> getMethods() {
+		if (methods == null)
+			build();
+		return methods;
+	}
+	private void build() {
+		List<String> classNames = Program.getDynamicallyLoadedClasses();
+        HostedVM.initialize();
+		methods = new IndexSet<jq_Method>();
+        for (String s : classNames) {
+			jq_Class c = Program.loadClass(s);
+			if (c == null)
+				continue;
+			for (jq_Method m : c.getDeclaredStaticMethods()) {
+				if (!m.isAbstract())
+					m.getCFG();
+				methods.add(m);
+			}
+			for (jq_Method m : c.getDeclaredInstanceMethods()) {
+				if (!m.isAbstract())
+					m.getCFG();
+				methods.add(m);
+			}
+		}
+	}
+}
