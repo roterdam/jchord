6e99b50c94cfccc474e164cf13d696cdc5932996
mayur.naik <mayur.naik@fd19b882-93bd-11dd-bc7b-59c4a08ba726>
2010-08-13 08:22:57 +0000

    more moving

--- main/src/java/chord/analyses/datarace/DataraceAnalysis.java
+++ main/src/java/chord/analyses/datarace/DataraceAnalysis.java
@@ -4,7 +4,7 @@
  * All rights reserved.
  * Licensed under the terms of the New BSD License.
  */
-package chord.analyses.datarace.statik;
+package chord.analyses.datarace;
 
 import java.io.PrintWriter;
 import java.util.HashMap;
