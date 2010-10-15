356fffa8ad899c02eca9985820e3b7c8fdd02c86
asrabkin <asrabkin@fd19b882-93bd-11dd-bc7b-59c4a08ba726>
2010-10-15 23:00:44 +0000

    Support defining classpath in via a second option, "extraPaths". (already added to Config.) This is to facilitate scripts that run a series of analyses differing on only a handful of paths.

--- main/src/java/chord/project/analyses/CoreDynamicAnalysis.java
+++ main/src/java/chord/project/analyses/CoreDynamicAnalysis.java
@@ -313,10 +313,14 @@ public class CoreDynamicAnalysis extends JavaAnalysis {
 		} else {
 			String bootClassesDirName = Config.bootClassesDirName;
 			String userClassesDirName = Config.userClassesDirName;
+	    String extraClasses = Config.extraClasses;
+	    if(extraClasses.length() > 0) {
+	      extraClasses = extraClasses + File.pathSeparator;
+	    }
 			basecmd.add("-Xbootclasspath/p:" + Config.mainClassPathName +
 				File.pathSeparator + bootClassesDirName);
 			basecmd.add("-cp");
-			basecmd.add(userClassesDirName + File.pathSeparator +
+			basecmd.add(extraClasses + userClassesDirName + File.pathSeparator +
 				classPathName);
 		}
 		{
