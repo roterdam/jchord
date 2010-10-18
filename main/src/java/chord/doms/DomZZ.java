d7f7f466d9424730b901f51d1e16d27c2578e123
asrabkin <asrabkin@fd19b882-93bd-11dd-bc7b-59c4a08ba726>
2010-10-18 03:07:11 +0000

    Another domain of integers, this time fixed size.

---
+++ main/src/java/chord/doms/DomZZ.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2008-2010, Intel Corporation.
+ * Copyright (c) 2006-2007, The Trustees of Stanford University.
+ * All rights reserved.
+ * Licensed under the terms of the New BSD License.
+ */
+package chord.doms;
+
+import chord.project.Chord;
+import chord.project.analyses.ProgramDom;
+/**
+ * Another domain of integers. Unlike domZ, has fixed size, unconnected
+ * to max_args.
+ *
+ */
+@Chord(
+    name = "ZZ"
+  )
+public class DomZZ extends ProgramDom<Integer> {
+  public static final int MAXZ = 32;
+  
+  @Override
+  public void fill() {
+    for (int i = 0; i < MAXZ; i++)
+      getOrAdd(new Integer(i));  
+  }
+}
