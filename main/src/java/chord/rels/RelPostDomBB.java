dcdc2083119c14aa3e2b76c036c9fbf7aa8971e8
mayur.naik <mayur.naik@fd19b882-93bd-11dd-bc7b-59c4a08ba726>
2010-10-24 22:46:50 +0000

    fixed a bug in computation of post dominance relation

--- main/src/java/chord/rels/RelPostDomBB.java
+++ main/src/java/chord/rels/RelPostDomBB.java
@@ -10,7 +10,6 @@ import chord.util.ArraySet;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Set;
-import java.util.List;
 
 import joeq.Class.jq_Method;
 import joeq.Class.jq_Class;
@@ -20,7 +19,7 @@ import chord.program.visitors.IMethodVisitor;
 import chord.program.Program;
 import chord.project.Chord;
 import chord.project.analyses.ProgramRel;
-import joeq.Util.Templates.ListIterator;
+import joeq.Util.Templates.List;
 
 /**
  * Relation containing each pair of basic blocks (b1,b2) in each method
@@ -42,33 +41,44 @@ public class RelPostDomBB extends ProgramRel implements IMethodVisitor {
 		// System.out.println("VISITING: " + m);
 		pdomMap.clear();
 		ControlFlowGraph cfg = m.getCFG();
+		BasicBlock exit = cfg.exit();
+		Set<BasicBlock> exitSet = new ArraySet<BasicBlock>(1);
+		exitSet.add(exit);
+		pdomMap.put(exit, exitSet);
+		List.BasicBlock rpo = cfg.reversePostOrder(cfg.entry());
+		int n = rpo.size();
+		Set<BasicBlock> initSet = new ArraySet<BasicBlock>(n);
+		for (int i = 0; i < n; i++) {
+			BasicBlock bb = rpo.getBasicBlock(i);
+			initSet.add(bb);
+		}
+		for (int i = 0; i < n; i++) {
+			BasicBlock bb = rpo.getBasicBlock(i);
+			if (bb != exit)
+				pdomMap.put(bb, initSet);
+		}
 		boolean changed;
 		while (true) {
 			changed = false;
-			for (ListIterator.BasicBlock it = cfg.reversePostOrderIterator(); it.hasNext();) {
-				BasicBlock bb = it.nextBasicBlock();
+			for (int i = n - 1; i >= 0; i--) {
+				BasicBlock bb = rpo.getBasicBlock(i);
+				if (bb == exit)
+					continue;
 				Set<BasicBlock> oldPdom = pdomMap.get(bb);
 				Set<BasicBlock> newPdom = null;
-				List<BasicBlock> succs = bb.getSuccessors();
-				int n = succs.size();
-				if (n >= 1) {
-					Set<BasicBlock> fstPdom = pdomMap.get(succs.get(0));
-					if (fstPdom != null) {
-						newPdom = new ArraySet<BasicBlock>(fstPdom);
-						for (int i = 1; i < n; i++) {
-							Set<BasicBlock> nxtPdom = pdomMap.get(succs.get(i));
-							if (nxtPdom == null) {
-								newPdom.clear();
-								break;
-							}
-							newPdom.retainAll(nxtPdom);
-						}
+				java.util.List<BasicBlock> succs = bb.getSuccessors();
+				int k = succs.size();
+				if (k >= 1) {
+					Set<BasicBlock> fst = pdomMap.get(succs.get(0));
+					newPdom = new ArraySet<BasicBlock>(fst);
+					for (int j = 1; j < k; j++) {
+						Set<BasicBlock> nxt = pdomMap.get(succs.get(j));
+						newPdom.retainAll(nxt);
 					}
-				}
-				if (newPdom == null)
+				} else
 					newPdom = new ArraySet<BasicBlock>(1);
 				newPdom.add(bb);
-				if (oldPdom == null || !oldPdom.equals(newPdom)) {
+				if (!oldPdom.equals(newPdom)) {
 					changed = true;
 					pdomMap.put(bb, newPdom);
 				}
@@ -82,7 +92,7 @@ public class RelPostDomBB extends ProgramRel implements IMethodVisitor {
 				// System.out.print(" " + bb2);
 				add(bb2, bb);
 			}
-			System.out.println();
+			// System.out.println();
 		}
 	}
 }
