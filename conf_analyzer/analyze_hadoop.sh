c53d9f89a1c9a8c9941c525fd634969e19ed0bdc
asrabkin <asrabkin@fd19b882-93bd-11dd-bc7b-59c4a08ba726>
2010-12-08 04:45:51 +0000

    revised hadoop analysis script

--- conf_analyzer/analyze_hadoop.sh
+++ conf_analyzer/analyze_hadoop.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
+#  This is a script for analyzing Hadoop.
+#  To use it, you need to set HADOOP_HOME below, or else have it in your environment
 #
 #  Modify the line below to point to your HADOOP_HOME.
 #
 HADOOP_HOME=/Users/asrabkin/workspace/hadoop-0.20.2
 
-ant run -Dchord.work.dir=`pwd`/examples/hadoop20 -DhadoopHome=$HADOOP_HOME -Ddictionary.name=hdfs.dict -Dchord.main.class=org.apache.hadoop.hdfs.server.datanode.DataNode -Dentrypoints.file=examples/hadoop20/entrypoints-20-hdfs.txt
+if [[ $0 != "/"* ]]; then
+HERE=`pwd`
+else
+HERE=`dirname $0`
+fi
+echo "script is in $HERE"
 
-ant run -Dchord.work.dir=`pwd`/examples/hadoop20 -DhadoopHome=$HADOOP_HOME -Ddictionary.name=mapred.dict -Dchord.main.class=org.apache.hadoop.mapred.TaskTracker -Dentrypoints.file=examples/hadoop20/entrypoints-20-mapred.txt
+ant run -Dchord.work.dir=$HERE/examples/hadoop20 \
+-Ddictionary.name=hdfs.dict \
+-Dchord.main.class=org.apache.hadoop.hdfs.server.datanode.DataNode \
+-Dhadoophome=$HADOOP_HOME \
+-Dentrypoints.file=examples/hadoop20/entrypoints-20-hdfs.txt \
+-Dchord.props.file=$HERE/examples/hadoop20/nomain.properties
+
+ant run -Dchord.work.dir=$HERE/examples/hadoop20 \
+-Ddictionary.name=mapred.dict \
+-Dchord.main.class=org.apache.hadoop.mapred.TaskTracker \
+-Dhadoophome=$HADOOP_HOME \
+-Dentrypoints.file=examples/hadoop20/entrypoints-20-mapred.txt \
+-Dchord.props.file=$HERE/examples/hadoop20/nomain.properties
