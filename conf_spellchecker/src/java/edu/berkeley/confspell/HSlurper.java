b930060dff275403ba980077fb9477fbefcdda33
asrabkin <asrabkin@fd19b882-93bd-11dd-bc7b-59c4a08ba726>
2010-12-24 00:49:21 +0000

    reformat spellchecker code; use tabs not spaces

--- conf_spellchecker/src/java/edu/berkeley/confspell/HSlurper.java
+++ conf_spellchecker/src/java/edu/berkeley/confspell/HSlurper.java
@@ -14,37 +14,36 @@ import edu.berkeley.confspell.SpellcheckConf.Slurper;
 /**
  * Slurper for Hadoop configuration files. Has a run-time and compile-time
  * dependency on Hadoop common components.
- *
+ * 
  */
 public class HSlurper implements Slurper {
 
-  //stolen from hadoop source
+	// stolen from hadoop source
 
-  
-  public void slurp(File f, OptionSet res) {
-    Configuration c = new Configuration(false);
-    c.addResource(new Path(f.getAbsolutePath())); //to search filesystem, not classpath
-    c.reloadConfiguration();
-    fromHConf(res, c);
-  }
+	public void slurp(File f, OptionSet res) {
+		Configuration c = new Configuration(false);
+		c.addResource(new Path(f.getAbsolutePath())); // to search filesystem, not
+																									// classpath
+		c.reloadConfiguration();
+		fromHConf(res, c);
+	}
 
+	public static void fromHConf(OptionSet res, Configuration c) {
+		for (Map.Entry<String, String> e : c) {
 
-  public static void fromHConf(OptionSet res, Configuration c) {
-    for(Map.Entry<String, String> e: c) {
-      
-      String rawV = e.getValue();
-      String cookedV =  c.get(e.getKey());
-      
-      res.put(e.getKey(), cookedV); //to force substitution
-      
-      res.checkForSubst(rawV);
-    }
-  }
-  
-  public static OptionSet fromHConf(Configuration c) {
-    OptionSet res = new OptionSet();
-    fromHConf(res, c);
-    return res;
-  }
- 
+			String rawV = e.getValue();
+			String cookedV = c.get(e.getKey());
+
+			res.put(e.getKey(), cookedV); // to force substitution
+
+			res.checkForSubst(rawV);
+		}
+	}
+
+	public static OptionSet fromHConf(Configuration c) {
+		OptionSet res = new OptionSet();
+		fromHConf(res, c);
+		return res;
+	}
+
 }
