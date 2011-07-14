319607bac1f92484808bb8d44b821aa0c8b3a331
mayur.naik <mayur.naik@fd19b882-93bd-11dd-bc7b-59c4a08ba726>
2011-07-14 22:21:25 +0000

    allow sign of a relation to be declared along with its name, using !sign=... after the name, instead of requiring namesOfSigns array in @chord annotation

--- main/src/chord/project/ChordAnnotParser.java
+++ main/src/chord/project/ChordAnnotParser.java
@@ -25,7 +25,19 @@ import chord.util.Utils;
  * @author Mayur Naik (mhn@cs.stanford.edu)
  */
 public class ChordAnnotParser {
-	private static final String ERROR = "ERROR: @Chord annotation of class '%s': %s";
+	private static final String ERROR = "ERROR: ChordAnnotParser: @Chord annotation of class '%s': %s";
+	private static final String SIGN_EMPTY = "Method sign() cannot return empty string.";
+	private static final String SIGN_NON_EMPTY = "Method sign() cannot return non-empty string.";
+	private static final String PRODUCES_NON_EMPTY = "Method produces() cannot return non-empty string.";
+	private static final String CONTROLS_NON_EMPTY = "Method controls() cannot return non-empty string.";
+	private static final String AMBIGUOUS_TYPE = "Method namesOfSigns() implicitly declares name '%s' as having type '%s' whereas method namesOfTypes() declares it as having incompatible type '%s'."; 
+	private static final String TYPES_ARYLEN_MISMATCH = "Methods namesOfTypes() and types() return arrays of different lengths.";
+	private static final String SIGNS_ARYLEN_MISMATCH = "Methods namesOfSigns() and signs() return arrays of different lengths.";
+	private static final String NAMES_OF_TYPES_FORBIDDEN = "Method namesOfTypes() cannot return the same name as that returned by name()";
+	private static final String NAMES_OF_TYPES_REPEATING = "Method namesOfTypes() cannot return a name ('%s') multiple times.";
+	private static final String NAMES_OF_SIGNS_FORBIDDEN = "Method namesOfSigns() cannot return the same name as that returned by name(); use sign().";
+	private static final String NAMES_OF_SIGNS_REPEATING = "Method namesOfSigns() cannot return a name ('%s') multiple times.";
+
 	private final Class type;
 	private String name;
 	private String prescriber;
@@ -35,6 +47,12 @@ public class ChordAnnotParser {
 	private Map<String, RelSign> nameToSignMap;
 	private Map<String, Class> nameToTypeMap;
 	private boolean hasNoErrors;
+	
+	private void error(String format, Object... args) {
+        String msg = String.format(format, args);
+        Messages.log(ERROR, type.getName(), msg);
+		hasNoErrors = false;
+	}
 	/**
 	 * Constructor.
 	 * 
@@ -54,23 +72,25 @@ public class ChordAnnotParser {
 		hasNoErrors = true;
 		name = chord.name();
 
-		prescriber = chord.prescriber();
-		if (prescriber.equals(""))
-			prescriber = name;
+		nameToTypeMap = new HashMap<String, Class>();
+		nameToTypeMap.put(name, type);
 
+		nameToSignMap = new HashMap<String, RelSign>();
 		String sign = chord.sign();
 		RelSign relSign = null;
 		if (Utils.isSubclass(type, ProgramRel.class)) {
-			if (sign.equals("")) {
-				error("Method sign() cannot return empty string " +
-					"for Java analysis '" + type + "'");
-			} else {
+			if (sign.equals(""))
+				error(SIGN_EMPTY);
+			else {
 				relSign = parseRelSign(sign);
+				nameToSignMap.put(name, relSign);
 			}
-		} else if (!sign.equals("")) {
-			error("Method sign() cannot return non-empty string " +
-				 "for Java analysis '" + type + "'");
-		}
+		} else if (!sign.equals(""))
+			error(SIGN_NON_EMPTY);
+
+		prescriber = chord.prescriber();
+		if (prescriber.equals(""))
+			prescriber = name;
 
 		{
 			String[] a = chord.consumes();
@@ -83,92 +103,75 @@ public class ChordAnnotParser {
 					consumes.add(domName);
 			}
 			for (String s : a)
-				consumes.add(s);
+				consumes.add(processName(s));
 		}
 
 		{
 			String[] a = chord.produces();
 			produces = new ArrayList<String>(a.length);
 			for (String s : a)
-				produces.add(s);
+				produces.add(processName(s));
 		}
 
 		{
 			String[] a = chord.controls();
 			controls = new ArrayList<String>(a.length);
 			for (String s : a)
-				controls.add(s);
+				controls.add(processName(s));
 		}
 
 		// program rels and doms should not declare any produces/controls
 		if (Utils.isSubclass(type, ProgramRel.class) ||
 			Utils.isSubclass(type, ProgramDom.class)) {
 			if (produces.size() > 0) {
-				error("Method produces() cannot return non-empty string " +
-					" for Java analysis '" + type + "'");
+				error(PRODUCES_NON_EMPTY);
 				produces.clear();
 			}
 			produces.add(name);
 			if (controls.size() > 0) {
-				error("Method controls() cannot return non-empty string " +
-					" for Java analysis '" + type + "'");
+				error(CONTROLS_NON_EMPTY);
 				controls.clear();
 			}
 		}
 
-		nameToTypeMap = new HashMap<String, Class>();
-		nameToTypeMap.put(name, type);
 		String[] namesOfTypes = chord.namesOfTypes();
 		Class [] types = chord.types();
 		if (namesOfTypes.length != types.length) {
-			error("Methods namesOfTypes() and types() " +
-				"return arrays of different lengths.");
+			error(TYPES_ARYLEN_MISMATCH);
 		} else {
 			for (int i = 0; i < namesOfTypes.length; i++) {
 				String name2 = namesOfTypes[i];
 				if (name2.equals(name) || name2.equals(".")) {
-					error("Method namesOfTypes() cannot return the same " +
-						"name as that returned by name()");
+					error(NAMES_OF_TYPES_FORBIDDEN);
 					continue;
 				}
 				if (nameToTypeMap.containsKey(name2)) {
-					error("Method namesOfTypes() cannot return a name ('" +
-						name2 + "') multiple times.");
+					error(NAMES_OF_TYPES_REPEATING, name2);
 					continue;
 				}
 				nameToTypeMap.put(name2, types[i]);
 			}
 		}
 
-		nameToSignMap = new HashMap<String, RelSign>();
-		if (relSign != null)
-			nameToSignMap.put(this.name, relSign);
 		String[] namesOfSigns = chord.namesOfSigns();
 		String[] signs = chord.signs();
 		if (namesOfSigns.length != signs.length) {
-			error("Methods namesOfSigns() and signs() " +
-				"return arrays of different lengths.");
+			error(SIGNS_ARYLEN_MISMATCH);
 		} else {
 			for (int i = 0; i < namesOfSigns.length; i++) {
 				String name2 = namesOfSigns[i];
 				if (name2.equals(name) || name2.equals(".")) {
-					error("Method namesOfSigns() cannot return the same " +
-						"name as that returned by name(); use sign().");
+					error(NAMES_OF_SIGNS_FORBIDDEN);
 					continue;
 				}
 				if (nameToSignMap.containsKey(name2)) {
-					error("Method namesOfSigns() cannot return a name ('" +
-						name2 + "') multiple times.");
+					error(NAMES_OF_SIGNS_REPEATING, name2);
 					continue;
 				}
 				Class type2 = nameToTypeMap.get(name2);
 				if (type2 != null) {
 					if (!Utils.isSubclass(type2, ProgramRel.class)) {
-						error("Method namesOfSigns() implicitly declares " +
-							"name '" + name2 + "' as having type '" +
-							ProgramRel.class.getName() + "' whereas method " +
-							"namesOfTypes() declares it as having " +
-							"incompatible type '" + type2.getName() + "'."); 
+						error(AMBIGUOUS_TYPE, name2, ProgramRel.class.getName(), type2.getName());
 						continue;
 					}
 				}
@@ -179,6 +182,22 @@ public class ChordAnnotParser {
 		}
 
 		return hasNoErrors;
+	}
+
+	private String processName(String s) {
+		int i = s.indexOf('!');
+		String name2;
+		if (i == -1)
+			name2 = s;
+		else {
+			name2 = s.substring(0, i);
+			String t = s.substring(i + 1);
+			if (t.startsWith("sign=")) {
+				RelSign relSign2 = parseRelSign(t.substring(5));
+				nameToSignMap.put(name2, relSign2);
+			}
+		}
+		return name2;
 	}
 	private RelSign parseRelSign(String sign) {
 		int i = sign.indexOf(':');
@@ -197,10 +216,6 @@ public class ChordAnnotParser {
 			error(ex.getMessage());
 			return null;
 		}
-	}
-	private void error(String msg) {
-		Messages.log(ERROR, type.getName(), msg);
-		hasNoErrors = false;
 	}
 	/**
 	 * Provides the name specified by this Chord annotation of the
