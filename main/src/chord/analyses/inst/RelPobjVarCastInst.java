c5c7ea3d0ca5201774adc0b8467fc331599580da
mayur.naik <mayur.naik@fd19b882-93bd-11dd-bc7b-59c4a08ba726>
2011-07-14 22:21:58 +0000

    better P* relations for all kinds of copy assignments, including move, phi, and cast

---
+++ main/src/chord/analyses/inst/RelPobjVarCastInst.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2008-2010, Intel Corporation.
+ * Copyright (c) 2006-2007, The Trustees of Stanford University.
+ * All rights reserved.
+ * Licensed under the terms of the New BSD License.
+ */
+package chord.analyses.inst;
+
+import joeq.Class.jq_Class;
+import joeq.Class.jq_Method;
+import joeq.Compiler.Quad.Operand;
+import joeq.Compiler.Quad.Quad;
+import joeq.Compiler.Quad.Operand.RegisterOperand;
+import joeq.Compiler.Quad.Operator.CheckCast;
+import joeq.Compiler.Quad.RegisterFactory.Register;
+
+import chord.program.visitors.ICastInstVisitor;
+import chord.project.Chord;
+import chord.project.analyses.ProgramRel;
+
+/**
+ *
+ * @author Mayur Naik (mhn@cs.stanford.edu)
+ */
+@Chord(name = "PobjVarCastInst", sign = "P0,V0,V1:P0_V0xV1")
+public class RelPobjVarCastInst extends ProgramRel implements ICastInstVisitor {
+	public void visit(jq_Class c) { }
+	public void visit(jq_Method m) { }
+	public void visitCastInst(Quad q) {
+        Operand rx = CheckCast.getSrc(q);
+        if (rx instanceof RegisterOperand) {
+			RegisterOperand ro = (RegisterOperand) rx;
+			if (ro.getType().isReferenceType()) {
+				Register r = ro.getRegister();
+				Register l = CheckCast.getDest(q).getRegister();
+				add(q, l, r);
+			}
+        }
+    }
+}
+
---
+++ main/src/chord/analyses/inst/RelPobjVarMoveInst.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2008-2010, Intel Corporation.
+ * Copyright (c) 2006-2007, The Trustees of Stanford University.
+ * All rights reserved.
+ * Licensed under the terms of the New BSD License.
+ */
+package chord.analyses.inst;
+
+import joeq.Class.jq_Class;
+import joeq.Class.jq_Method;
+import joeq.Compiler.Quad.Operand;
+import joeq.Compiler.Quad.Quad;
+import joeq.Compiler.Quad.Operand.RegisterOperand;
+import joeq.Compiler.Quad.Operator.Move;
+import joeq.Compiler.Quad.RegisterFactory.Register;
+
+import chord.program.visitors.IMoveInstVisitor;
+import chord.project.Chord;
+import chord.project.analyses.ProgramRel;
+
+/**
+ *
+ * @author Mayur Naik (mhn@cs.stanford.edu)
+ */
+@Chord(name = "PobjVarMoveInst", sign = "P0,V0,V1:P0_V0xV1")
+public class RelPobjVarMoveInst extends ProgramRel implements IMoveInstVisitor {
+	public void visit(jq_Class c) { }
+	public void visit(jq_Method m) { }
+	public void visitMoveInst(Quad q) {
+		Operand rx = Move.getSrc(q);
+		if (rx instanceof RegisterOperand) {
+			RegisterOperand ro = (RegisterOperand) rx;
+			if (ro.getType().isReferenceType()) {
+				Register r = ro.getRegister();
+				Register l = Move.getDest(q).getRegister();
+				add(q, l, r);
+			}
+		}
+	}
+}
+
