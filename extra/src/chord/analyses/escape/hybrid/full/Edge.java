9811ecca5f7a11e1ca5a02aead55572fcd94cdc3
mayur.naik <mayur.naik@fd19b882-93bd-11dd-bc7b-59c4a08ba726>
2012-02-13 01:53:27 +0000

    added support for chord.escape.both; most changes are formatting

--- extra/src/chord/analyses/escape/hybrid/full/Edge.java
+++ extra/src/chord/analyses/escape/hybrid/full/Edge.java
@@ -17,66 +17,62 @@ import chord.project.analyses.rhs.IEdge;
  * @author Mayur Naik (mhn@cs.stanford.edu)
  */
 public class Edge implements IEdge {
-	final SrcNode srcNode;
-	// dstNode is intentionally not final: it is updated when this edge
-	// is merged with another edge with matching srcNode; see mergeWith
-	DstNode dstNode;
-	public Edge(SrcNode s, DstNode d) {
-		srcNode = s;
-		dstNode = d;
-	}
-	@Override
-	public boolean canMerge(IEdge pe2) {
-		SrcNode srcNode2 = ((Edge) pe2).srcNode;
-		if (!srcNode.equals(srcNode2))
-			return false;
-		if (ThreadEscapeFullAnalysis.join != Join.PJOIN)
-			return true;
-		DstNode dstNode1 = this.dstNode;
-		DstNode dstNode2 = ((Edge) pe2).dstNode;
-		boolean isRetn1 = dstNode1.isRetn;
-		boolean isRetn2 = dstNode2.isRetn;
-		assert (isRetn1 == isRetn2);
-		// boolean isKill1 = dstNode1.isKill;
-		// boolean isKill2 = dstNode2.isKill;
-		// if (isKill1 != isKill2) {
-		//	return false;
-		// }
+    final SrcNode srcNode;
+    // dstNode is intentionally not final: it is updated when this edge
+    // is merged with another edge with matching srcNode; see mergeWith
+    DstNode dstNode;
+    public Edge(SrcNode s, DstNode d) {
+        srcNode = s;
+        dstNode = d;
+    }
+    @Override
+    public boolean canMerge(IEdge pe2) {
+        SrcNode srcNode2 = ((Edge) pe2).srcNode;
+        if (!srcNode.equals(srcNode2)) return false;
+        if (ThreadEscapeFullAnalysis.join != Join.PJOIN) return true;
+        DstNode dstNode1 = this.dstNode;
+        DstNode dstNode2 = ((Edge) pe2).dstNode;
+        boolean isRetn1 = dstNode1.isRetn;
+        boolean isRetn2 = dstNode2.isRetn;
+        assert (isRetn1 == isRetn2);
+        // boolean isKill1 = dstNode1.isKill;
+        // boolean isKill2 = dstNode2.isKill;
+        // if (isKill1 != isKill2) {
+        //    return false;
+        // }
         Obj[] env1 = dstNode1.env;
         Obj[] env2 = dstNode2.env;
         int n = env1.length;
         assert (n == env2.length);
-		// 'bigger' remains 0 as long as dstNode1 == dstNode2
-		// it's value switches to 1 when dstNode1 < dstNode2
-		// it's value switches to 2 when dstNode2 > dstNode1
-		// we return false if both 1 and 2 above hold
-		int bigger = 0;
+        // 'bigger' remains 0 as long as dstNode1 == dstNode2
+        // it's value switches to 1 when dstNode1 < dstNode2
+        // it's value switches to 2 when dstNode2 > dstNode1
+        // we return false if both 1 and 2 above hold
+        int bigger = 0;
         for (int i = 0; i < n; i++) {
-			Obj pts1 = env1[i];
+            Obj pts1 = env1[i];
             Obj pts2 = env2[i];
-			if (pts1 == pts2) continue;
-			if (pts1 == Obj.EMTY || pts2 == Obj.BOTH) {
-				if (bigger == 1) return false;
-				bigger = 2;
-				continue;
-			}
-			if (pts2 == Obj.EMTY || pts1 == Obj.BOTH) {
-				if (bigger == 2) return false;
-				bigger = 1;
-				continue;
-			}
-			if ((pts1 == Obj.ONLY_ESC && pts2 == Obj.ONLY_LOC) ||
-			    (pts2 == Obj.ONLY_ESC && pts1 == Obj.ONLY_LOC))
-				return false;
-			System.out.println("FAILED");
-			throw new RuntimeException();
-		}
+            if (pts1 == pts2) continue;
+            if (pts1 == Obj.EMTY || pts2 == Obj.BOTH) {
+                if (bigger == 1) return false;
+                bigger = 2;
+                continue;
+            }
+            if (pts2 == Obj.EMTY || pts1 == Obj.BOTH) {
+                if (bigger == 2) return false;
+                bigger = 1;
+                continue;
+            }
+            if ((pts1 == Obj.ONLY_ESC && pts2 == Obj.ONLY_LOC) ||
+                (pts2 == Obj.ONLY_ESC && pts1 == Obj.ONLY_LOC))
+                return false;
+            throw new RuntimeException("FAILED");
+        }
         ArraySet<FldObj> heap1 = dstNode1.heap;
         ArraySet<FldObj> heap2 = dstNode2.heap;
-		if (heap1.equals(heap2))
-			return true;
-		int n1 = heap1.size();
-		int n2 = heap2.size();
+        if (heap1.equals(heap2)) return true;
+        int n1 = heap1.size();
+        int n2 = heap2.size();
         for (int i = 0; i < n1; i++) {
             FldObj fo1 = heap1.get(i);
             jq_Field f = fo1.f;
@@ -84,35 +80,35 @@ public class Edge implements IEdge {
             for (int j = 0; j < n2; j++) {
                 FldObj fo2 = heap2.get(j);
                 if (fo2.f == f) {
-					boolean isLoc1 = fo1.isLoc;
-					boolean isEsc1 = fo1.isEsc;
+                    boolean isLoc1 = fo1.isLoc;
+                    boolean isEsc1 = fo1.isEsc;
                     boolean isLoc2 = fo2.isLoc;
                     boolean isEsc2 = fo2.isEsc;
                     if (isLoc1 == isLoc2 && isEsc1 == isEsc2) {
                         ;
-					} else if (isLoc1 && isEsc1) {
-						if (bigger == 2) return false;
-						bigger = 1;
-					} else if (isLoc2 && isEsc2) {
-						if (bigger == 1) return false;
-						bigger = 2;
-					} else {
-						if ((!isLoc1 && !isEsc1) ||
-							(!isLoc2 && !isEsc2)) {
-							System.out.println("FAILED");
-							throw new RuntimeException();
-						}
-						return false;
-					}
+                    } else if (isLoc1 && isEsc1) {
+                        if (bigger == 2) return false;
+                        bigger = 1;
+                    } else if (isLoc2 && isEsc2) {
+                        if (bigger == 1) return false;
+                        bigger = 2;
+                    } else {
+                        if ((!isLoc1 && !isEsc1) ||
+                            (!isLoc2 && !isEsc2)) {
+                            System.out.println("FAILED");
+                            throw new RuntimeException();
+                        }
+                        return false;
+                    }
                     found = true;
                     break;
                 }
             }
-			if (!found) {
-				if (bigger == 2) return false;
-				bigger = 1;
-			}
-		}
+            if (!found) {
+                if (bigger == 2) return false;
+                bigger = 1;
+            }
+        }
         for (int j = 0; j < n2; j++) {
             FldObj fo2 = heap2.get(j);
             jq_Field f = fo2.f;
@@ -125,140 +121,135 @@ public class Edge implements IEdge {
                 }
             }
             if (!found) {
-				if (bigger == 1)
-					return false;
-				else
-					return true;
-			}
+                return (bigger == 1) ? false : true;
+            }
         }
-		return true;
-	}
-	@Override
-	public boolean mergeWith(IEdge pe2) {
-		DstNode dstNode1 = this.dstNode;
-		DstNode dstNode2 = ((Edge) pe2).dstNode;
-		boolean isRetn1 = dstNode1.isRetn;
-		boolean isRetn2 = dstNode2.isRetn;
-		assert (isRetn1 == isRetn2);
+        return true;
+    }
+    @Override
+    public boolean mergeWith(IEdge pe2) {
+        DstNode dstNode1 = this.dstNode;
+        DstNode dstNode2 = ((Edge) pe2).dstNode;
+        boolean isRetn1 = dstNode1.isRetn;
+        boolean isRetn2 = dstNode2.isRetn;
+        assert (isRetn1 == isRetn2);
         boolean changed = false;
-		boolean isKill1 = dstNode1.isKill;
-		boolean isKill2 = dstNode2.isKill;
-		if (!isKill1 && isKill2) {
-			isKill1 = true;
-           	changed = true;
+        boolean isKill1 = dstNode1.isKill;
+        boolean isKill2 = dstNode2.isKill;
+        if (!isKill1 && isKill2) {
+            isKill1 = true;
+            changed = true;
         }
-		// merge env's
+        // merge env's
         Obj[] env1 = dstNode1.env;
         Obj[] env2 = dstNode2.env;
         int n = env1.length;
         assert (n == env2.length);
         Obj[] env3 = null;
         for (int i = 0; i < n; i++) {
-			Obj pts1 = env1[i];
+            Obj pts1 = env1[i];
             Obj pts2 = env2[i];
             if (pts1 == pts2 || pts1 == Obj.BOTH || pts2 == Obj.EMTY) {
-				if (env3 != null)
-					env3[i] = pts1;
-				continue;
-			}
-			// if reached here then need to allocate env3 if it hasn't
-			// already been allocated
-			if (pts1 == Obj.EMTY)
-				pts1 = pts2;
-			else {
-				// at this point there are only two cases:
-				// pts1=LOC and pts2=ESC|BOTH
-				// pts1=ESC and pts2=LOC|BOTH
-				pts1 = Obj.BOTH;
-			}
-			if (env3 == null) {
-				env3 = new Obj[n];
-				for (int j = 0; j < i; j++)
-					env3[j] = env1[j];
-			}
-			env3[i] = pts1;
+                if (env3 != null)
+                    env3[i] = pts1;
+                continue;
+            }
+            // if reached here then need to allocate env3 if it hasn't
+            // already been allocated
+            if (pts1 == Obj.EMTY)
+                pts1 = pts2;
+            else {
+                // at this point there are only two cases:
+                // pts1=LOC and pts2=ESC|BOTH
+                // pts1=ESC and pts2=LOC|BOTH
+                pts1 = Obj.BOTH;
+            }
+            if (env3 == null) {
+                env3 = new Obj[n];
+                for (int j = 0; j < i; j++)
+                    env3[j] = env1[j];
+            }
+            env3[i] = pts1;
         }
         if (env3 != null) {
             env1 = env3;
             changed = true;
         }
-		// merge heaps
+        // merge heaps
         ArraySet<FldObj> heap1 = dstNode1.heap;
         ArraySet<FldObj> heap2 = dstNode2.heap;
         if (!heap1.equals(heap2)) {
-			heap1 = mergeHeaps(heap1, heap2);
+            heap1 = mergeHeaps(heap1, heap2);
             changed = true;
         }
-		if (changed) {
-			this.dstNode = new DstNode(env1, heap1, isKill1, isRetn1);
-			return true;
-		}
+        if (changed) {
+            this.dstNode = new DstNode(env1, heap1, isKill1, isRetn1);
+            return true;
+        }
         return false;
-	}
-	private static ArraySet<FldObj> mergeHeaps(ArraySet<FldObj> heap1,
-			ArraySet<FldObj> heap2) {
-		int n1 = heap1.size();
-		int n2 = heap2.size();
-		ArraySet<FldObj> heap3 = new ArraySet<FldObj>();
-		for (int i = 0; i < n1; i++) {
-			FldObj fo1 = heap1.get(i);
-			jq_Field f = fo1.f;
-			boolean found = false;
-			for (int j = 0; j < n2; j++) {
-				FldObj fo2 = heap2.get(j);
-				if (fo2.f == f) {
-					boolean isLoc1 = fo1.isLoc;
-					boolean isEsc1 = fo1.isEsc;
-					boolean isLoc2 = fo2.isLoc;
-					boolean isEsc2 = fo2.isEsc;
-					if (isLoc1 == isLoc2 && isEsc1 == isEsc2)
-						heap3.add(fo1);
-					else if (isLoc1 && isEsc1)
-						heap3.add(fo1);
-					else if (isLoc2 && isEsc2)
-						heap3.add(fo2);
-					else
-						heap3.add(new FldObj(f, true, true));
-					found = true;
-					break;
-				}
-			}
-			if (!found)
-				heap3.add(fo1);
-		}
-		for (int j = 0; j < n2; j++) {
-			FldObj fo2 = heap2.get(j);
-			jq_Field f = fo2.f;
-			boolean found = false;
-			for (int i = 0; i < n1; i++) {
-				FldObj fo1 = heap1.get(i);
-				if (fo1.f == f) {
-					found = true;
-					break;
-				}
-			}
-			if (!found)
-				heap3.add(fo2);
-		}
-		return heap3;
-	}
+    }
+    private static ArraySet<FldObj> mergeHeaps(ArraySet<FldObj> heap1,
+            ArraySet<FldObj> heap2) {
+        int n1 = heap1.size();
+        int n2 = heap2.size();
+        ArraySet<FldObj> heap3 = new ArraySet<FldObj>();
+        for (int i = 0; i < n1; i++) {
+            FldObj fo1 = heap1.get(i);
+            jq_Field f = fo1.f;
+            boolean found = false;
+            for (int j = 0; j < n2; j++) {
+                FldObj fo2 = heap2.get(j);
+                if (fo2.f == f) {
+                    boolean isLoc1 = fo1.isLoc;
+                    boolean isEsc1 = fo1.isEsc;
+                    boolean isLoc2 = fo2.isLoc;
+                    boolean isEsc2 = fo2.isEsc;
+                    if (isLoc1 == isLoc2 && isEsc1 == isEsc2)
+                    heap3.add(fo1);
+                    else if (isLoc1 && isEsc1)
+                        heap3.add(fo1);
+                    else if (isLoc2 && isEsc2)
+                        heap3.add(fo2);
+                    else
+                        heap3.add(new FldObj(f, true, true));
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+                heap3.add(fo1);
+        }
+        for (int j = 0; j < n2; j++) {
+            FldObj fo2 = heap2.get(j);
+            jq_Field f = fo2.f;
+            boolean found = false;
+            for (int i = 0; i < n1; i++) {
+                FldObj fo1 = heap1.get(i);
+                if (fo1.f == f) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+                heap3.add(fo2);
+        }
+        return heap3;
+    }
 
-	public int hashCode() {
+    public int hashCode() {
         int i = 5381;
         i = ((i << 5) + i) + srcNode.hashCode();
         i = ((i << 5) + i) + dstNode.hashCode();
-		return i;
-	}
-	public boolean equals(Object o) {
-		if (o == this)
-			return true;
-		if (!(o instanceof Edge))
-			return false;
-		Edge that = (Edge) o;
-		return srcNode.equals(that.srcNode) && dstNode.equals(that.dstNode);
-	}
-	public String toString() {
-		return srcNode + ";" + dstNode;
-	}
+        return i;
+    }
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Edge)) return false;
+        Edge that = (Edge) o;
+        return srcNode.equals(that.srcNode) && dstNode.equals(that.dstNode);
+    }
+    public String toString() {
+        return srcNode + ";" + dstNode;
+    }
 }
 
--- extra/src/chord/analyses/escape/hybrid/full/SrcNode.java
+++ extra/src/chord/analyses/escape/hybrid/full/SrcNode.java
@@ -7,7 +7,6 @@
 package chord.analyses.escape.hybrid.full;
 
 import java.util.Arrays;
-
 import chord.util.ArraySet;
 
 /**
@@ -15,29 +14,27 @@ import chord.util.ArraySet;
  * @author Mayur Naik (mhn@cs.stanford.edu)
  */
 public class SrcNode {
-	final Obj[] env;
-	final ArraySet<FldObj> heap;
-	public SrcNode(Obj[] env, ArraySet<FldObj> heap) {
-		this.env = env;
-		this.heap = heap;
-	}
-	public int hashCode() {
+    final Obj[] env;
+    final ArraySet<FldObj> heap;
+    public SrcNode(Obj[] env, ArraySet<FldObj> heap) {
+        this.env = env;
+        this.heap = heap;
+    }
+    public int hashCode() {
         int i = 5381;
         for (Obj pts : env) {
             i = ((i << 5) + i) + pts.hashCode();
         }
         return i;
-	}
-	public boolean equals(Object o) {
-		if (o == this)
-			return true;
-		if (!(o instanceof SrcNode))
-			return false;
-		SrcNode that = (SrcNode) o;
-		return Arrays.equals(env, that.env)&&heap.equals(that.heap);
-	}
-	public String toString() {
-		return "v@s=" + ThreadEscapeFullAnalysis.toString(env) +
-			 "; h@s=" + ThreadEscapeFullAnalysis.toString(heap);
-	}
+    }
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof SrcNode)) return false;
+        SrcNode that = (SrcNode) o;
+        return Arrays.equals(env, that.env) && heap.equals(that.heap);
+    }
+    public String toString() {
+        return "v@s=" + ThreadEscapeFullAnalysis.toString(env) +
+             "; h@s=" + ThreadEscapeFullAnalysis.toString(heap);
+    }
 }
