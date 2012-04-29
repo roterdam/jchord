7c5cd5436b05589523c391a63fcbfabd386c805d
mayur.naik <mayur.naik@fd19b882-93bd-11dd-bc7b-59c4a08ba726>
2012-04-29 15:46:47 +0000

    minor import

--- extra/src/chord/analyses/typestate/TypeStateAnalysis.java
+++ extra/src/chord/analyses/typestate/TypeStateAnalysis.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Iterator;
 
 import joeq.Class.jq_Field;
 import joeq.Class.jq_Method;
